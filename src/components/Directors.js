272717442dedcadd435475720fc5ba96f5c1e111
morgancollado <Morgan.collado@gmail.com>
2021-01-21 19:33:34 -0600

    commit tree 5b032600b40e683fc3b69c8f54e6c1529de88b03

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,26 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+
+  const dir = directors.map(d => {
+    const mov = d.movies.map(m => {
+      return(
+        <li>{m}</li>
+      )
+    })
+    return (
+      <div>
+        <h3>{d.name}</h3>
+        <ul>
+          {mov}
+        </ul>
+      </div>
+    )
+  })
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {dir}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,25 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+  const mov = movies.map(m => {
+    const genre = m.genres.map(g => {
+      return <li>{g}</li>
+    })
+    return(
+      <div>
+        <h2>{m.title}</h2>
+        <p>Run time: {m.time}</p>
+        <ul>
+          {genre}
+        </ul>
+      </div>
+    )
+  })
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Pages</h1>
+        {mov}
     </div>
   );
 };
